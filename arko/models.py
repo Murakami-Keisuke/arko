0e51b4a90ad1fac798e9d897a48a13e82c927083
Murakami-Keisuke <alcedo_atthis_ksk@yahoo.co.jp>
2022-02-18 21:52:41 +0900

    delete tkinter?

--- arko/models.py
+++ arko/models.py
@@ -1,5 +1,5 @@
 from email.policy import default
-from tkinter import E
+# from tkinter import E
 from tokenize import group
 from django.db import models
 from django.contrib.auth.models import User,Group,AbstractUser
