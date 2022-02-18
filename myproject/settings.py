f25b19eb3dfa676a4f399f28a6f3616354fb2bd9
Murakami-Keisuke <alcedo_atthis_ksk@yahoo.co.jp>
2022-02-18 23:20:57 +0900

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,9 +137,10 @@ STATICFILES_DIRS = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# static
 DEBUG = False
 
 try:
