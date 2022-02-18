682e936983f9cb153858f714baa37f2d8d3ac6ff
Murakami-Keisuke <alcedo_atthis_ksk@yahoo.co.jp>
2022-02-19 01:12:15 +0900

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -152,5 +152,6 @@ if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
