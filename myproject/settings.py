5de228bdca0f60e610bb25a4b12910fc10b2a13d
Murakami-Keisuke <alcedo_atthis_ksk@yahoo.co.jp>
2022-02-19 08:01:53 +0900

    fixed

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/') # 追記
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
