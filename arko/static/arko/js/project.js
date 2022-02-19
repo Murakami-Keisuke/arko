c2bc55a32b0278a7e43ecffd9e4f9b3cfdf3b23c
Murakami-Keisuke <alcedo_atthis_ksk@yahoo.co.jp>
2022-02-19 11:26:06 +0900

    fixed

--- arko/static/arko/js/project.js
+++ arko/static/arko/js/project.js
@@ -9,7 +9,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var API_host = 'http://127.0.0.1:8000/';
+var API_host = 'https://arkoapp.herokuapp.com/';
 var e = React.createElement;
 
 var room_selected = '';
